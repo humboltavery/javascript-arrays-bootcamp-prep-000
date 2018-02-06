f199685ab37f681abd0dc10383d130eed88b7a8c
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-06 23:27:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (array, el) {
+  return [el, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray (array, el) {
+  array.unshift(el);
+  return array;
+}
+
+function addElementToEndOfArray(array, el) {
+  return [array, ...el];
+}
